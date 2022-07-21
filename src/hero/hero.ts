6e58eee8b8f0efadc3d71288661b9013f30b0d94
Bohdan <qwebeck5@gmail.com>
2022-07-21 18:16:55 +0200

    Adds jumps

--- src/hero/hero.ts
+++ src/hero/hero.ts
@@ -10,10 +10,12 @@ export class Hero {
   public position: Vector2D;
   public width = 70;
   public height = 96;
-  private moving = 10;
+  private moving = 30;
+  private initialY: number;
 
   constructor(x: number, y: number) {
     this.position = new Vector2D(x, y);
+    this.initialY = y;
   }
 
   public move(speed: Vector2D) {
@@ -39,23 +41,23 @@ export class Hero {
 
   public jump(speed: Vector2D) {
     //if (this.position.y > Environemnt.worldSizeY - 300) {
-      this.position.y += speed.y 
+    this.position.y += speed.y
     //} 
     //else if (this.position.y < Environemnt.worldSizeY - 300 && this.position.y > Environemnt.worldSizeY * 0.8)
-    
+
     // else if (this.position.y = Environemnt.worldSizeY - 299) {
     //   this.position.y += Environemnt.gravity.y;
     // // } else if (this.position.y <= Environemnt.worldSizeY * 0.9 + this.height) {
     // //   this.position.y += speed.y
     // // }
     // } 
-    
+
     // else if (this.isJumping === false || this.position.y > Environemnt.worldSizeY * 0.8) {
     //   this.position.y += Environemnt.gravity
     // }
-}
+  }
 
-  public pickUp() {}
+  public pickUp() { }
 
   public update(weather: Weather) {
     const speed = this.handleKeyDown();
@@ -64,45 +66,35 @@ export class Hero {
   }
 
   public handleKeyDown() {
+    let speed = new Vector2D(0, 0);
     if (keyIsDown(LEFT_ARROW)) {
-      return new Vector2D(-this.moving, 0);
+      speed = speed.add(new Vector2D(-this.moving, 0));
     }
     if (keyIsDown(RIGHT_ARROW)) {
-      return new Vector2D(this.moving, 0);
+      speed = speed.add(new Vector2D(this.moving, 0));
     }
-    // if (keyIsDown(UP_ARROW)) {
-    //   return new Vector2D(0, -this.moving);
-    // }
-    // if (keyIsDown(DOWN_ARROW)) {
-    //   return new Vector2D(0, this.moving);
-    // }
-
     if (keyIsDown(UP_ARROW)) {
       this.isJumping = true
     }
 
-    if (this.isJumping) {
-      console.log("true")
-      return new Vector2D(2, -this.moving);
-    }
-
-    if (this.position.y < Environemnt.worldSizeY - 300) {
+    if (this.isJumping && this.position.y > Environemnt.worldSizeY - 300) {
+      speed = speed.add(new Vector2D(2, -this.moving));
+    } else {
       this.isJumping = false
     }
 
-    if (this.isJumping === false) {
-      console.log("false")
-      return new Vector2D(2, Environemnt.gravity)
+    if (this.isJumping === false && this.position.y < this.initialY) {
+      speed = speed.add(new Vector2D(2, this.moving))
     }
 
-    return new Vector2D(0, 0);
+    return speed;
   }
-  
+
   animate() {
     this.sprite.animation.play();
   }
 
   public show() {
-   this.sprite.position()
+    this.sprite.position()
   }
- }
+}
